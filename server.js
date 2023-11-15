8166a30a4c9ed2804ae64eb124d404e61d792c87
David Gomez <dgz2903@gmail.com>
2023-11-14 19:26:39 -0600

    Desafio clase 6

--- server.js
+++ server.js
@@ -1,24 +1,24 @@
 const express = require('express');
 const app = express();
-const PORT = 5000;
+const PORT = 8080;
 
 const Productos = [
-  { id: 1, name: 'Producto 1' },
-  { id: 2, name: 'Producto 2' },
-  { id: 3, name: 'Producto 3' },
-  { id: 4, name: 'Producto 4' },
-  { id: 5, name: 'Producto 5' },
-  { id: 6, name: 'Producto 6' },
-  { id: 7, name: 'Productoo 7' },
-  { id: 8, name: 'Producto 8' },
-  { id: 9, name: 'Producto 9' },
-  { id: 10, name: 'Producto 10' }
+  { id: 1, nombre: 'Producto 1' },
+  { id: 2, nombre: 'Producto 2' },
+  { id: 3, nombre: 'Producto 3' },
+  { id: 4, nombre: 'Producto 4' },
+  { id: 5, nombre: 'Producto 5' },
+  { id: 6, nombre: 'Producto 6' },
+  { id: 7, nombre: 'Productoo 7' },
+  { id: 8, nombre: 'Producto 8' },
+  { id: 9, nombre: 'Producto 9' },
+  { id: 10, nombre: 'Producto 10' }
 ];
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
-// Route to get all Productos
-app.get('/productos', (req, res) => {
+
+app.get('/products', (req, res) => {
   const { limit } = req.query;
   if (limit) {
     res.json(Productos.slice(0, limit));
@@ -27,7 +27,7 @@ app.get('/productos', (req, res) => {
   }
 });
 
-app.get('/productos/:id', (req, res) => {
+app.get('/products/:id', (req, res) => {
   const ProductoId = parseInt(req.params.id);
   const Producto = Productos.find(p => p.id === ProductoId);
 
